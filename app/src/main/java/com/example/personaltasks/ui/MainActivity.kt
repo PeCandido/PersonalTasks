05163cc0a5e95c196e9c7b9c901d8e93d6fd6296
Pedro Candido Salvio <pedrocsalvio@gmail.com>
2025-05-22 13:24:13 -0300

    feat: inicializing RecycleView in MainActivity

--- app/src/main/java/com/example/personaltasks/ui/MainActivity.kt
+++ app/src/main/java/com/example/personaltasks/ui/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.personaltasks.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.example.personaltasks.adapter.TaskAdapter
 import com.example.personaltasks.data.AppDatabase
+import com.example.personaltasks.model.Task
 import com.exemplo.personaltasks.R
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var taskAdapter: TaskAdapter
+    private lateinit var db: AppDatabase
+    private var tasks: List<Task> = listOf()
 
     private val taskDao by lazy {
         AppDatabase.getDatabase(this).taskDAO()
@@ -14,5 +21,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recyclerView = findViewById(R.id.rv_taks)
+
+        db = AppDatabase.getDatabase(this)
+
+        taskAdapter = TaskAdapter(tasks) {
+            view, task ->
+        }
+
+        recyclerView.adapter = taskAdapter
     }
 }

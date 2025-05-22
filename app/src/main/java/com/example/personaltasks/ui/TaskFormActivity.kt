b7f4640fc312f7155c643573c18d453fd63b3ded
Pedro Candido Salvio <pedrocsalvio@gmail.com>
2025-05-22 15:32:59 -0300

    feat: implementing showDatePicker method in TaskFormActivity

--- app/src/main/java/com/example/personaltasks/ui/TaskFormActivity.kt
+++ app/src/main/java/com/example/personaltasks/ui/TaskFormActivity.kt
@@ -1,5 +1,6 @@
 package com.example.personaltasks.ui
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
@@ -43,6 +44,25 @@ class TaskFormActivity : AppCompatActivity() {
         if (taskId != 0) {
             loadTask()
         }
+
+        dateEdit.setOnClickListener {
+            showDatePicker()
+        }
+    }
+
+    private fun showDatePicker() {
+        val dateSetListener = DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
+            calendar.set(year, month, dayOfMonth)
+            dateEdit.setText(dateFormat.format(calendar.time))
+        }
+
+        DatePickerDialog(
+            this,
+            dateSetListener,
+            calendar.get(Calendar.YEAR),
+            calendar.get(Calendar.MONTH),
+            calendar.get(Calendar.DAY_OF_MONTH)
+        ).show()
     }
 
     private fun loadTask() {

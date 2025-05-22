0f3432c28c6e20a7d6ea78d8344d0ef2d4787941
Pedro Candido Salvio <pedrocsalvio@gmail.com>
2025-05-21 22:58:33 -0300

    feat: creating AppDatabase class

---
+++ app/src/main/java/com/example/personaltasks/data/AppDatabase.kt
@@ -0,0 +1,30 @@
+package com.example.personaltasks.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.personaltasks.model.Task
+
+@Database(entities = [Task::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun taskDAO(): TaskDAO
+
+    companion object{
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "personal_tasks_db"
+                ).build()
+
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}

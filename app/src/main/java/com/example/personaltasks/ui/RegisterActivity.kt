0732ccd4091db911548181242734b862da65dd81
Pedro Candido <pedrocsalvio@gmail.com>
2025-06-25 19:27:52 -0300

    feat: implementing performRegistration method in RegisterActivity to register account

--- app/src/main/java/com/example/personaltasks/ui/RegisterActivity.kt
+++ app/src/main/java/com/example/personaltasks/ui/RegisterActivity.kt
@@ -1,9 +1,11 @@
 package com.example.personaltasks.ui
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ProgressBar
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.personaltasks.R
 import com.google.firebase.auth.FirebaseAuth
@@ -27,7 +29,34 @@ class RegisterActivity: AppCompatActivity() {
         registerButton = findViewById(R.id.register_btn)
 
         registerButton.setOnClickListener{
-
+            performRegistration()
         }
+    }
+
+    private fun performRegistration() {
+        val email = emailEdit.text.toString().trim()
+        val password = passwordEdit.text.toString().trim()
+
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Fill all the fields", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if(task.isSuccessful) {
+                    Toast.makeText(this, "Account Registered!", Toast.LENGTH_SHORT).show()
+
+                    val intent = Intent(this, MainActivity::class.java)
+
+                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                    startActivity(intent)
+                    finish()
+
+                } else {
+                    val errorMessage = task.exception?.message ?: "Error"
+                    Toast.makeText(this, "Failure to register: $errorMessage", Toast.LENGTH_LONG).show()
+                }
+            }
     }
 }

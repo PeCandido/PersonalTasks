e5d33317e046cf83d76419a6e6d8f241be3e4091
Pedro Candido <pedrocsalvio@gmail.com>
2025-06-25 17:44:12 -0300

    refactor: altering Task class with userID and isDeleted boolean var

--- app/src/main/java/com/example/personaltasks/model/Task.kt
+++ app/src/main/java/com/example/personaltasks/model/Task.kt
@@ -2,13 +2,16 @@ package com.example.personaltasks.model
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.google.firebase.firestore.DocumentId
 
 // Tabela no banco de dados que armazena as tarefas
-@Entity(tableName = "tasks")
 class Task (
-    @PrimaryKey(autoGenerate = true) val id: Int = 0,
-    val title: String,
-    val description: String,
-    val deadline: String,
-    val isDone: Boolean
+    @DocumentId
+    var id: Int = 0,
+    var userId: String? = null,
+    var title: String = "",
+    var description: String = "",
+    var deadline: String = "",
+    var isDone: Boolean = false,
+    var isDeleted: Boolean = false
 )

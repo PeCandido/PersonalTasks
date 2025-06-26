bbdcd0c00a251b596b5db04dbda557f8057c2af1
Pedro Candido <pedrocsalvio@gmail.com>
2025-06-26 16:39:47 -0300

    feat: implementing new constructor to Task class

--- app/src/main/java/com/example/personaltasks/model/Task.kt
+++ app/src/main/java/com/example/personaltasks/model/Task.kt
@@ -4,14 +4,15 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.google.firebase.firestore.DocumentId
 
-// Tabela no banco de dados que armazena as tarefas
-class Task (
+data class Task(
     @DocumentId
-    var id: Int = 0,
+    var id: String? = null,
     var userId: String? = null,
     var title: String = "",
     var description: String = "",
     var deadline: String = "",
     var isDone: Boolean = false,
     var isDeleted: Boolean = false
-)
+) {
+    constructor() : this(null, null, "", "", "", false, false)
+}

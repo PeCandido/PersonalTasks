7cfb134396ce549b6b2fe9806e5fba0b933485ce
Pedro Candido <pedrocsalvio@gmail.com>
2025-06-26 17:15:32 -0300

    feat: creating DeletedTasksActivity for removed tasks page

---
+++ app/src/main/java/com/example/personaltasks/ui/DeletedTasksActivity.kt
@@ -0,0 +1,31 @@
+package com.example.personaltasks.ui
+
+import android.os.Bundle
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.recyclerview.widget.RecyclerView
+import com.example.personaltasks.R
+import com.example.personaltasks.adapter.TaskAdapter
+import com.example.personaltasks.model.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+
+class DeletedTasksActivity: AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var deletedTaskAdapter: TaskAdapter
+    private var selectedTask: Task? = null
+
+    private val firestore = FirebaseFirestore.getInstance()
+    private val auth = FirebaseAuth.getInstance()
+    private val tasksCollection = firestore.collection("tasks")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_deleted_tasks)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar_deleted)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+}
